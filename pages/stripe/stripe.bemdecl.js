b93c9f36c651db262ef4dc1972cdf4f25084fd13
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-01-28 14:38:21 +0300

    Add bemdecl

---
+++ pages/stripe/stripe.bemdecl.js
@@ -0,0 +1,14 @@
+exports.blocks = [
+    {
+        "name": "stripe",
+        "elems": [
+            {"name": "close"}
+        ],
+        "mods": [
+            {
+                "name": "template",
+                "vals": [{"name": "simple"}]
+            }
+        ]
+    }
+];
